f3ab71f5e79fcb66643c73229e98dc9bb6e7e546
Daniel Yong <danielysj790@gmail.com>
2026-09-02 21:36:44 +0800

    feat: enhance MongoDB connection handling to return database client

--- backend/services/mongodb/mongodb_connect.py
+++ backend/services/mongodb/mongodb_connect.py
@@ -15,8 +15,15 @@ def connect_to_mongodb():
     except Exception as e:
         raise ConnectionError(f"Failed to connect to MongoDB: {e}")
     
-    return client
+    client_database_name = os.getenv("MONGODB_DATABASE_NAME", None)
+    
+    if not client_database_name:
+        raise ValueError("MONGODB_DATABASE_NAME must be set in the environment variables.")
+    
+    database_client = client[client_database_name]
+
+    return client, database_client
 
 if __name__ == "__main__":
-    client = connect_to_mongodb()
+    _ = connect_to_mongodb()
     print("Successfully connected to MongoDB.")
